6dcf03c4592286f0e0177b09ca6a8937682ba3ca
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-18 16:05:20 -0500

    Animated cube

--- src/script.js
+++ src/script.js
@@ -28,4 +28,18 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Animations
+const tick = () => 
+{
+    // Update objects
+    mesh.rotation.y += 0.01
+    
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
